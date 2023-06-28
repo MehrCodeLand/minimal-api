54e608704b6f47924d307f29013377d355723630
MehrCodeLand <mehrcodeland@gmail.com>
2023-06-29 00:36:53 +0330

    Add project files.

---
+++ UserDD/dbo/StoredProcedures/spuser_GetAll.sql
@@ -0,0 +1,6 @@
+﻿CREATE PROCEDURE [dbo].[spuser_GetAll]
+AS
+begin
+	select Id , FirstName , LastName
+	from dbo.[User];
+end
---
+++ UserDD/dbo/Tables/User.sql
@@ -0,0 +1,6 @@
+﻿CREATE TABLE [dbo].[User]
+(
+	[Id] INT NOT NULL PRIMARY KEY IDENTITY(0, 5), 
+    [FirstName] NVARCHAR(50) NULL, 
+    [LastName] NVARCHAR(50) NULL
+)
